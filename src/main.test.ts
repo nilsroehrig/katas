0249f4b981a5184707db4cac5a56c8ae7c4ec9c5
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2025-03-29 18:06:58 +0100

    add second test case

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,25 +1,54 @@
-import {beforeEach, describe, expect, test} from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
 
-type RollValue = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
-type BasicRoll = { type: "roll", value: RollValue };
-type StrikeRoll = { type: "strike-roll", value: 10 }
+type RollValue = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+type BasicRoll = { type: "roll"; value: RollValue };
+type StrikeRoll = { type: "strike-roll"; value: 10 };
 type Roll = BasicRoll | StrikeRoll;
 
-type PartialFrame = { type: "partial", rolls: [Roll] }
-type BasicFrame = { type: "basic", rolls: [Roll, Roll] }
-type Spare = { type: "spare", rolls: [Roll, Roll] }
-type Strike = { type: "strike", rolls: [StrikeRoll] }
-type FinalStrike = { type: "final-strike", rolls: [StrikeRoll, Roll] | [StrikeRoll, StrikeRoll, Roll] }
-type FinalSpare = { type: "final-spare", rolls: [Roll, Roll, Roll] | [Roll, Roll, StrikeRoll] }
+type PartialFrame = { type: "partial"; rolls: [Roll] };
+type BasicFrame = { type: "basic"; rolls: [Roll, Roll] };
+type Spare = { type: "spare"; rolls: [Roll, Roll] };
+type Strike = { type: "strike"; rolls: [StrikeRoll] };
+type FinalStrike = {
+  type: "final-strike";
+  rolls: [StrikeRoll, Roll] | [StrikeRoll, StrikeRoll, Roll];
+};
+type FinalSpare = {
+  type: "final-spare";
+  rolls: [Roll, Roll, Roll] | [Roll, Roll, StrikeRoll];
+};
 
-type Frame = PartialFrame | BasicFrame | Spare | Strike | FinalStrike | FinalSpare;
+type Frame =
+  | PartialFrame
+  | BasicFrame
+  | Spare
+  | Strike
+  | FinalStrike
+  | FinalSpare;
 
-type Game = { frames: Frame[] }
+type Game = { frames: Frame[] };
 
 function roll(game: Game, rollValue: RollValue): Game {
-  return {
-    frames: [makeFrame([makeRoll(rollValue)])]
+  const [latestFrame, ...previousFrames] = game.frames.toReversed();
+
+  if (!latestFrame) {
+    return {
+      frames: [makeFrame([makeRoll(rollValue)])],
+    };
   }
+
+  if (latestFrame.type === "partial") {
+    return {
+      frames: [
+        ...previousFrames.toReversed(),
+        makeFrame([...latestFrame.rolls, makeRoll(rollValue)]),
+      ],
+    };
+  }
+
+  return {
+    frames: [makeFrame([makeRoll(rollValue)])],
+  };
 }
 
 function score(game: Game): number {
@@ -28,22 +57,42 @@ function score(game: Game): number {
 
 function makeRoll(value: RollValue): Roll {
   if (value === 10) {
-    return {type: "strike-roll", value}
+    return { type: "strike-roll", value };
   }
-  return {type: "roll", value}
+
+  return { type: "roll", value };
 }
 
 function makeFrame(rolls: Frame["rolls"]): Frame {
-  return {type: "partial", rolls} as PartialFrame
+  const [, secondRoll] = rolls;
+  if (!secondRoll) {
+    return { type: "partial", rolls } as Frame;
+  }
+
+  return { type: "basic", rolls } as Frame;
 }
 
 describe("roll", () => {
   let game: Game;
 
-  beforeEach(() => game = {frames: []})
+  beforeEach(() => (game = { frames: [] }));
 
-  test('adds a partial frame on non-strike roll', () => {
+  test("adds a partial frame on non-strike roll", () => {
     const nextGame = roll(game, 0);
-    expect(nextGame.frames).toEqual([{type: "partial", rolls: [makeRoll(0)]}])
-  })
-})
+    expect(nextGame.frames).toEqual([
+      { type: "partial", rolls: [makeRoll(0)] },
+    ]);
+  });
+
+  test("completes a partial frame on second roll", () => {
+    const firstRoll = makeRoll(0);
+
+    game.frames.push(makeFrame([firstRoll]));
+
+    const nextGame = roll(game, 1);
+
+    expect(nextGame.frames).toEqual([
+      { type: "basic", rolls: [firstRoll, makeRoll(1)] } as Frame,
+    ]);
+  });
+});
