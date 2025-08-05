5ad520d83f22563d99542bec800aa7f8b19bfc54
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2025-08-06 01:38:32 +0200

    add test verifying using a mana slot works

--- tcg-2025-08-06/src/main.test.ts
+++ tcg-2025-08-06/src/main.test.ts
@@ -6,10 +6,18 @@ describe("ManaSlot", () => {
     const mana_slot = new ManaSlot();
     expect(mana_slot.empty).toBe(true);
   })
+
   test("should not be empty after refill", () => {
     const mana_slot = new ManaSlot();
     mana_slot.refill();
     expect(mana_slot.empty).toBe(false);
+  })
+
+  test("should be empty after a filled slot is used", () => {
+    const mana_slot = new ManaSlot();
+    mana_slot.refill();
+    mana_slot.use();
+    expect(mana_slot.empty).toBe(true);
   })
 })
 
@@ -18,6 +26,10 @@ class ManaSlot {
 
   refill(): void {
     this._empty = false;
+  }
+
+  use(): void {
+    this._empty = true;
   }
 
   get empty(): boolean {
