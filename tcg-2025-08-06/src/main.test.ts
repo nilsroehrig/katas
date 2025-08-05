aea5d08d474e98f607fadc37bb7602473fa1cc40
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2025-08-06 01:56:02 +0200

    cannot add more than 10 mana slots to player

--- tcg-2025-08-06/src/main.test.ts
+++ tcg-2025-08-06/src/main.test.ts
@@ -1,11 +1,24 @@
 import {describe, expect, test} from "vitest";
 import {ManaSlot} from "./domain/ManaSlot";
 
-describe.todo("Player", () => {
+describe("Player", () => {
   test("should increase mana slots when mana slot is received", () => {
     const player = new Player();
     player.receive_mana_slot();
     expect(player.mana_slots).toHaveLength(1);
+  })
+
+  test("should not receive more than 10 mana slots", () => {
+    const player = new Player();
+    for (let i = 0; i < 10; i++) {
+      player.receive_mana_slot();
+    }
+
+    expect(player.mana_slots).toHaveLength(10);
+
+    player.receive_mana_slot();
+
+    expect(player.mana_slots).toHaveLength(10);
   })
 });
 
@@ -13,13 +26,13 @@ class Player {
   private _mana_slots: ManaSlot[] = [];
 
   receive_mana_slot(): void {
+    if (this._mana_slots.length >= 10) {
+      return;
+    }
     this._mana_slots.push(new ManaSlot());
   }
 
   get mana_slots () {
-    return Object.freeze(this._mana_slots);
+    return this._mana_slots;
   }
 }
-
-
-
