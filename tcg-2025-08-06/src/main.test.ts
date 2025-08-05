76fcb847224ae3143dc4b85f8dca1c3e14335048
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2025-08-06 01:49:27 +0200

    add test verifying mana slots are added when a mana slot is received

--- tcg-2025-08-06/src/main.test.ts
+++ tcg-2025-08-06/src/main.test.ts
@@ -1,6 +1,25 @@
 import {describe, expect, test} from "vitest";
+import {ManaSlot} from "./domain/ManaSlot";
 
-describe.todo("next test");
+describe.todo("Player", () => {
+  test("should increase mana slots when mana slot is received", () => {
+    const player = new Player();
+    player.receive_mana_slot();
+    expect(player.mana_slots).toHaveLength(1);
+  })
+});
+
+class Player {
+  private _mana_slots: ManaSlot[] = [];
+
+  receive_mana_slot(): void {
+    this._mana_slots.push(new ManaSlot());
+  }
+
+  get mana_slots () {
+    return Object.freeze(this._mana_slots);
+  }
+}
 
 
 
