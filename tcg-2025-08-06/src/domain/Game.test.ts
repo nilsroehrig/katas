3fb1c22776341cafc4606a31a476e2f17de6bb7a
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2025-08-07 00:10:46 +0200

    declares the opponent player as winner if the active player bleeds out

--- tcg-2025-08-06/src/domain/Game.test.ts
+++ tcg-2025-08-06/src/domain/Game.test.ts
@@ -1,4 +1,4 @@
-import {describe, expect, test} from "vitest";
+import {describe, expect, test, vi} from "vitest";
 import {Player} from "./Player";
 import {Deck} from "./Deck";
 
@@ -21,6 +21,21 @@ describe("Game", () => {
     expect(game.active_player).toBe(player2);
     expect(game.opponent_player).toBe(player1);
   })
+
+  test("should declared the opponent player as winner when the active placer bleeds out", () => {
+    const winning_layer = new Player();
+    const losing_player = new Player();
+
+    losing_player.take_damage({amount: 29});
+
+    const on_game_over = vi.fn();
+
+    const game = new Game(winning_layer, losing_player, on_game_over);
+
+    game.end_turn();
+
+    expect(on_game_over).toHaveBeenCalledWith(winning_layer, losing_player);
+  })
 })
 
 class Game {
@@ -29,7 +44,8 @@ class Game {
 
   constructor(
     initial_active_player: Player,
-    initial_opponent_player: Player
+    initial_opponent_player: Player,
+    private _on_game_over = (winner: Player, loser: Player) => {}
   ) {
     this._active_player = initial_active_player;
     this._opponent_player = initial_opponent_player;
@@ -39,6 +55,27 @@ class Game {
     const next_active_player = this._opponent_player;
     this._opponent_player = this._active_player;
     this._active_player = next_active_player;
+
+    this._active_player.draw_card();
+
+    switch(this.choose_winner()) {
+      case this.active_player:
+        this._on_game_over(this.active_player, this.opponent_player);
+        break;
+      case this.opponent_player:
+        this._on_game_over(this.opponent_player, this.active_player);
+        break;
+    }
+  }
+
+  choose_winner(): Player | null {
+    if (this.active_player.remaining_health <= 0) {
+      return this.opponent_player;
+    }
+    if (this.opponent_player.remaining_health <= 0) {
+      return this.active_player;
+    }
+    return null;
   }
 
   get active_player(): Player {
