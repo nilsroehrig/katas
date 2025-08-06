16fe0e332e3aa21de924eac2d50743f30288f7fe
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2025-08-06 22:14:42 +0200

    a random card can be taken from the deck

--- tcg-2025-08-06/src/domain/Deck.test.ts
+++ tcg-2025-08-06/src/domain/Deck.test.ts
@@ -11,6 +11,21 @@ describe("Deck", () => {
     expect(deck.max_cards).toBe(3);
     expect(deck.remaining_cards).toBe(3);
   })
+
+  test("should take a random card from the deck", () => {
+    let cards = [
+      new Card({amount: 0}),
+      new Card({amount: 4}),
+      new Card({amount: 8}),
+    ];
+
+    const deck = new Deck([...cards]);
+
+    const random_card = deck.take_random_card();
+    expect(cards).toContain(random_card);
+    expect(deck.remaining_cards).toBe(2);
+    expect(deck.max_cards).toBe(3);
+  })
 })
 
 class Deck {
@@ -21,10 +36,15 @@ class Deck {
   }
 
   get max_cards(): number {
-    return this._cards.length;
+    return this._max_cards;
   }
 
   get remaining_cards(): number {
     return this._cards.length;
+  }
+
+  take_random_card() {
+    const random_index = Math.floor(Math.random() * this._cards.length);
+    return this._cards.splice(random_index, 1)[0];
   }
 }
